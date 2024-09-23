b46ed57283c6b2abc174c5a7d30d29db763b1d19
marti <marti@LAPTOP-Q3J3RQ19>
2024-09-23 20:48:09 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1-2024C2-12C/Paciente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_2024C2_12C
+{
+    internal class Paciente
+    {
+        public int DNI { get; set; }
+        public String apellido { get; set; }
+    }
+}
---
+++ ConsoleApp1-2024C2-12C/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1_2024C2_12C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Paciente p = new Paciente();
+            p.apellido = "lopez";
+        }
+    }
+}
